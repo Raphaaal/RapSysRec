267fee2ad1c76128d26a573555ff065185128e63
Raphaaal <azorin.raphael@gmail.com>
2020-09-27 22:42:28 +0200

    Return object in addition to print

--- neo4j_handler.py
+++ neo4j_handler.py
@@ -13,16 +13,25 @@ class Neo4JHandler:
         with self.driver.session() as session:
             artist = session.write_transaction(self._create_artist, name, urn, popularity)
             print(artist)
+            return artist
 
     def create_print_feat(self, urn1, urn2, track_id, track_name):
         with self.driver.session() as session:
             feat = session.write_transaction(self._create_feat, urn1, urn2, track_id, track_name)
             print(feat)
+            return feat
 
     def get_print_artist(self, urn):
         with self.driver.session() as session:
             artist = session.write_transaction(self._get_artist, urn)
             print(artist)
+            return artist
+
+    def get_print_feat(self, track_id):
+        with self.driver.session() as session:
+            feat = session.write_transaction(self._get_feat, track_id)
+            print(feat)
+            return feat
 
     @staticmethod
     def _create_artist(tx, name, urn, popularity):
@@ -49,6 +58,17 @@ class Neo4JHandler:
         return [row[0] for row in result]
 
     @staticmethod
+    def _get_feat(tx, track_id):
+        result = tx.run(
+            "MATCH (a:Artist)-[r:FEAT]->(b:Artist) "
+            "WHERE r.track_id = $track_id "
+            "RETURN 'Feat ' + r.track_id + ' found with name ' + r.track_name",
+            track_id=track_id,
+        )
+        return [row[0] for row in result]
+
+    @staticmethod
+    # TODO: add feat date
     def _create_feat(tx, urn1, urn2, track_id, track_name):
         result = tx.run(
             "MATCH (a:Artist), (b:Artist) "
@@ -67,5 +87,7 @@ if __name__ == "__main__":
     graph = Neo4JHandler("bolt://localhost:7687", "neo4j", "root")
     graph.create_print_artist("La Fouine", 1, 37)
     graph.create_print_artist("Booba", 2, 86)
+    graph.get_print_artist(1)
     graph.create_print_feat(1, 2, "abcd", "Reste en chien")
+    graph.get_print_feat("abcd")
     graph.close()
